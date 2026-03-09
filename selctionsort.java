441b37cb14c7cc69e1dc63904ac5453a9244934e
JAYANTH-devolper <jayanths507@gmail.com>
2026-03-09 16:14:44 +0530

    INSERTION SORT

---
+++ selctionsort.java
@@ -0,0 +1,23 @@
+/* SELECTION SORT
+ */
+
+public class selctionsort {
+    public static void main(String[] args) {
+        int[] arr={9,8,7,4,5,1};
+        for(int i=0;i<arr.length-1;i++){
+            int min=i;
+            for(int j=i+1;j<arr.length;j++){
+                if(arr[j]<arr[min]){
+                    min=j;
+                }
+            }
+            int temp=arr[i];
+            arr[i]=arr[min];
+            arr[min]=temp;
+        }
+          for(int i=0;i<arr.length;i++){
+            System.out.print(arr[i]+" ");
+        }
+    }
+    
+}
