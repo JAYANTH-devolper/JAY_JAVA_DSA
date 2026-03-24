5e6ccd27c0934f87599ab4f9146b533cea9d1371
JAYANTH-devolper <jayanths507@gmail.com>
2026-03-24 18:10:00 +0530

    public class SudokusolverRecursion {
        public void solveSudoku(char[][] board) {
            solve(board);
        }
    
        private boolean solve(char[][] board) {
            for (int row = 0; row < board.length; row++) {
                for (int col = 0; col < board[0].length; col++) {
    
                    if (board[row][col] == '.') {
    
                        for (char num = '1'; num <= '9'; num++) {
    
                            if (isValid(board, row, col, num)) {
    
                                board[row][col] = num;
    
                                if (solve(board)) {
                                    return true;
                                }
    
                                // Backtrack
                           cd c:\Users\ganes\OneDrive\Desktop\DSA_JAVA\ ; if (True) { javac SudokusolverRecursion.java } ; if (True) { java SudokusolverRecursion }

---
+++ SudokusolverRecursion.java
@@ -0,0 +1,81 @@
+public class SudokusolverRecursion {
+
+    public void solveSudoku(char[][] board) {
+        solve(board);
+    }
+
+    private boolean solve(char[][] board) {
+        for (int row = 0; row < board.length; row++) {
+            for (int col = 0; col < board[0].length; col++) {
+
+                if (board[row][col] == '.') {
+
+                    for (char num = '1'; num <= '9'; num++) {
+
+                        if (isValid(board, row, col, num)) {
+
+                            board[row][col] = num;
+
+                            if (solve(board)) {
+                                return true;
+                            }
+
+                            // Backtrack
+                            board[row][col] = '.';
+                        }
+                    }
+
+                    return false; // No valid number found
+                }
+            }
+        }
+        return true; // Board solved
+    }
+
+    private boolean isValid(char[][] board, int row, int col, char num) {
+
+        for (int i = 0; i < 9; i++) {
+
+            // Check row
+            if (board[row][i] == num) return false;
+
+            // Check column
+            if (board[i][col] == num) return false;
+
+            // Check 3x3 box
+            int boxRow = 3 * (row / 3) + i / 3;
+            int boxCol = 3 * (col / 3) + i % 3;
+
+            if (board[boxRow][boxCol] == num) return false;
+        }
+
+        return true;
+    }
+
+    // ✅ ADD THIS (you were missing execution + output)
+    public static void main(String[] args) {
+
+        char[][] board = {
+            {'5','3','.','.','7','.','.','.','.'},
+            {'6','.','.','1','9','5','.','.','.'},
+            {'.','9','8','.','.','.','.','6','.'},
+            {'8','.','.','.','6','.','.','.','3'},
+            {'4','.','.','8','.','3','.','.','1'},
+            {'7','.','.','.','2','.','.','.','6'},
+            {'.','6','.','.','.','.','2','8','.'},
+            {'.','.','.','4','1','9','.','.','5'},
+            {'.','.','.','.','8','.','.','7','9'}
+        };
+
+        SudokusolverRecursion solver = new SudokusolverRecursion();
+        solver.solveSudoku(board);
+
+        // Print solved board
+        for (int i = 0; i < 9; i++) {
+            for (int j = 0; j < 9; j++) {
+                System.out.print(board[i][j] + " ");
+            }
+            System.out.println();
+        }
+    }
+}
