ca09de903543662b94c56b5256731b3908b31743
JAYANTH-devolper <jayanths507@gmail.com>
2026-04-07 16:08:02 +0530

    LongestRepeatingCharReplacement

---
+++ LongestRepeatingCharReplacement.java
@@ -0,0 +1,35 @@
+public class LongestRepeatingCharReplacement {
+
+    public static void main(String[] args) {
+
+        String s = "AABABBA";
+        int k = 1;
+
+        int result = characterReplacement(s, k);
+        System.out.println("Longest Length: " + result);
+    }
+
+    public static int characterReplacement(String s, int k) {
+
+        int[] freq = new int[26];
+
+        int left = 0;
+        int maxCount = 0;
+        int maxLength = 0;
+
+        for (int right = 0; right < s.length(); right++) {
+
+            freq[s.charAt(right) - 'A']++;
+            maxCount = Math.max(maxCount, freq[s.charAt(right) - 'A']);
+
+            if ((right - left + 1) - maxCount > k) {
+                freq[s.charAt(left) - 'A']--;
+                left++;
+            }
+
+            maxLength = Math.max(maxLength, right - left + 1);
+        }
+
+        return maxLength;
+    }
+}
